774d266889a94497d765b8d2863173fe82cefb80
Михаил Косточкин <kostochkin2002@inbox.ru>
2022-04-20 20:57:35 +0700

    Каркас 1.2

--- ConsolePostMachine/Machine.cs
+++ ConsolePostMachine/Machine.cs
@@ -4,7 +4,48 @@ using System.Text;
 
 namespace ConsolePostMachine
 {
+	class Tape
+	{
+		DoublyNode currentNode;
+		
+		public Tape()
+		{
+			currentNode = new DoublyNode(0, false);
+			DoublyNode node = currentNode;
+			for (int i = -1; i >= -10; i--)
+			{
+				node.leftNode = new DoublyNode(i, false);
+				node = node.leftNode;
+			}
+			node = currentNode;
+			for (int i = 1; i <= 10; i++)
+			{
+				node.rightNode = new DoublyNode(i, false);
+				node = node.rightNode;
+			}
+			currentNode = node;
+		}
+		public void MoveLeft()
+		{
+			currentNode = currentNode.leftNode;
+			//Если левая пустая, то...
+		}
+		public <int, bool>
+	}
+	class DoublyNode
+	{
+		public int index;
+		public bool value;
+		public DoublyNode leftNode;
+		public DoublyNode rightNode;
+		public DoublyNode(int _index, bool _value)
+		{
+			index = _index;
+			value = _value;
+		}
+	}
 	class Machine
 	{
+
 	}
 }
--- ConsolePostMachine/Program.cs
+++ ConsolePostMachine/Program.cs
@@ -8,14 +8,19 @@ namespace ConsolePostMachine
 	{
 		static void Main(string[] args)
 		{
-			var desktopPath = Environment.GetFolderPath(Environment.SpecialFolder.DesktopDirectory);
-			var filePath = Path.Combine(desktopPath, "commands.txt");
-			List<Command> commands = CommandInterpreter.TxtToCommands(filePath);
-			int num = 1;
-			foreach (Command command in commands)
+			//var desktopPath = Environment.GetFolderPath(Environment.SpecialFolder.DesktopDirectory);
+			//var filePath = Path.Combine(desktopPath, "commands.txt");
+			//List<Command> commands = CommandInterpreter.TxtToCommands(filePath);
+			//int num = 1;
+			//foreach (Command command in commands)
+			//{
+			//	Console.WriteLine(num + " " + command.GetInfo());
+			//	num++;
+			//}
+			Tape tape = new Tape();
+			for (int i = 0; i < length; i++)
 			{
-				Console.WriteLine(num + " " + command.GetInfo());
-				num++;
+
 			}
 		}
 	}
