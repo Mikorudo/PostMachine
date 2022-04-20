90d700766822e3b9c711abfbdc8edd91f24730a0
Михаил Косточкин <kostochkin2002@inbox.ru>
2022-04-20 13:20:36 +0700

    Добавьте файлы проекта.

---
+++ ConsolePostMachine/CommandInterpreter.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsolePostMachine
+{
+	delegate void Command(int firstLine, int secondLine);
+	class CommandInterpreter
+	{
+		private List<string> commands;
+		private int commandCount;
+		public IEnumerable<string> GetCommandText(int max)
+		{
+			for (int i = 0; i < max; i++)
+			{
+				if (i == commands.Count)
+				{
+
+				}
+			}
+		}
+		public CommandInterpreter(string path)
+		{
+			commandCount = 0;
+			using (StreamReader reader = new StreamReader(path))
+			{
+				string? line;
+				while ((line = reader.ReadLine()) != null)
+				{
+					commands.Add(line);
+					commandCount++;
+				}
+			}
+		}
+
+	}
+}
---
+++ ConsolePostMachine/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ConsolePostMachine
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine("Hello World!");
+		}
+	}
+}
